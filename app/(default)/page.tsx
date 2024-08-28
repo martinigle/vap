64ed9b4eff99a47db787dca3545065558e06402c
martinigle <martin.iglesias.adro@gmail.com>
2024-08-28 10:27:35 -0300

    minor fixes: favicon, description and title

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Simple',
-  description: 'Page description',
+  title: 'VAP',
+  description: 'Ventanas Alta Prestacion',
 }
 
 import Hero from '@/components/hero'
